016e953051ab6321d6b360e4bddde9888e3821b8
lushbit <lushbitdev@gmail.com>
2026-05-12 20:46:06 +0200

    Prepare Hytems 1.7.0 for Hytale pre5.7

--- src/main/java/dev/lushbit/hytems/ui/hud/PinnedItemsInventoryTracker.java
+++ src/main/java/dev/lushbit/hytems/ui/hud/PinnedItemsInventoryTracker.java
@@ -7,7 +7,7 @@ import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.inventory.InventoryComponent;
-import com.hypixel.hytale.server.core.inventory.InventoryChangeEvent;
+import com.hypixel.hytale.server.core.event.events.ecs.InventoryChangeEvent;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import dev.lushbit.hytems.HytemsPlugin;
